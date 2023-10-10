d9d7f0577422de1151864baeac49470ed760627a
Venkatesan <anuj.venkatesan@fiserv.com>
2023-10-10 15:01:14 -0400

    made changes

--- solution.java
+++ solution.java
@@ -1,6 +1,10 @@
 public class solution{
     public static boolean isPrime(int i)
     {
+        if(i == 0 || i == 1)
+        {
+            return false;
+        }
         for(int j = 2; j <=i/2;j++)
         {
             if(i%j == 0)
@@ -12,7 +16,11 @@ public class solution{
     }
     public static void main(String[] args)
     {
-        System.out.print(isPrime(21));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(0));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(31));
+
     }
     
     
